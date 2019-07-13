fbfa558dc81c7e4c9990fafdd18b5b50022ee859
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-07-13 15:03:30 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
         .setTitle('HELP BOX')
         .setThumbnail(client.user.avatarURL)
         .addField("Prefix:", "b!")
-        .addField("General", `**ping: **Get the latency of the bot\n**server: **Get the serverinfo\n**user: **Get the userinfo\n**ava: **Get the avatar of a user\n**stats: **Get the stats of the bot\n**info: **Get the info of the bot\n**invite: **Get the invte of the bot\n**help: **Get this help message`)
+        .addField("General", `**ping: **Get the latency of the bot\n**server: **Get the serverinfo\n**user: **Get the userinfo\n**ava: **Get the avatar of a user\n**stats: **Get the stats of the bot\n**info: **Get the info of the bot\n**invite: **Get the invte of the bot\n**vote: **Vote Bisly on bot sites\n**help: **Get this help message`)
         .addField("Fun", `**xkcd: **Get xkcd web comics\n**norris: **Get chuck norris jokes\n**joke: **Get a random joke\n**say: **Make the bot say something\n**rps: **Play rock-paper-scissors with the bot\n**catfact: **Get facts about cats\n**meme: **Get memes\n**emotes: **Get all custom emotes available in the server\n**emoji: **Resize any emoji\n**8ball: **Ask the bot your questions and get quick answers\n**ascii: **Style your messages\n**spam: **spam messages in spam channels`)
         .addField("Moderation", `**ban: **Ban someone with a reason\n**kick: **Kick someone with a reason\n**prune: **Purge messages in the channel\n**report: **Report someone with a reason\n**createchannel: **Create channels in the server\n**deletechannel: **Delete channels from the server`)
         .addField("Animal", `**dog: **Get cute doggos\n**cat: **Get cute kittens\n**shibe: **Get cute shibes\n**panda: **Get cute pandas\n**redpanda: **Get cute red pandas\n**fox: **Get foxes`)
