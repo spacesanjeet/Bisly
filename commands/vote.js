f39ac4c2448e69091d5e002b3576145a05eed6e9
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-07-13 14:59:44 +0530

    Create vote.js
    added the vote file.

---
+++ commands/vote.js
@@ -0,0 +1,15 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports = {
+	name: 'vote',
+	description: 'Vote the bot.',
+  execute(client, message, args) {
+    	let embed = new RichEmbed()
+    	.setTitle("Vote the bot here")
+    	.setColor('#F70827')
+    	.addField('Bisly on discordbot.org', '[• Here](https://discordbots.org/bot/496198253193461792/vote)')
+    	.addField('Bisly on botsfordiscord.com', '[• Here](https://botsfordiscord.com/bot/496198253193461792/vote)')
+    	.setTimestamp(new Date())
+    	message.channel.send(embed)
+    },
+};
