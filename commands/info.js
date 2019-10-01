24091b34b8be5fd36b51b00ad5af42144573735a
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-01 15:50:51 +0530

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -5,11 +5,11 @@ module.exports = {
     description: 'get info about the bot',
     execute(client, message, args) {
     	let embed = new RichEmbed()
-    	.setTitle("Bisly v2.0.1")
+    	.setTitle("Bisly v2.0.2")
     	.setColor('#F70827')
     	.setDescription(`Bisly is a general purpose discord bot which is developed with the view of performing all general tasks in mind.\n
     	The bot contains commands for **Fun**, **Moderation**, **Misc** and **General** tasks.\n
-        In this update, the bot is added up with a vote command with few bug fixes.`)
+        In this update, the bot is added up with a GIF command.`)
     	.addField("Webpage:", "https://bisly.glitch.me")
     	.addField("Github repository:", "[Bisly on Github](https://github.com/spacesanjeet/Bisly)")
     	.setFooter("spacesanjeet#1363")
