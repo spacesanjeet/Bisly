aac4d795eed67daa9d6ba20d4b48c9e4f8b1142f
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-09 17:20:31 +0530

    Update ev.js

--- commands/ev.js
+++ commands/ev.js
@@ -6,10 +6,12 @@ module.exports = {
     execute(client, message, args) {
         if(message.author.id !== process.env.OwnerId) return message.channel.send("You are not the developer!");
         try {
+            let evaled = eval(message.content.split(" ").slice(1).join(" "))
             let embed = new RichEmbed()
                 .setTitle("REPL")
-                .setDescription(eval(message.content.split(" ").slice(1).join(" ")));
-
+                .setDescription(evaled);
+            
+                console.log(evaled)
                 message.channel.send(embed)
                 message.react("right_emoji");
         }       catch(e) {
