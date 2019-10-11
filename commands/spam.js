020810c22890a167e5947917e92843f0c09889d9
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-11 13:09:09 +0530

    Update spam.js
    updated spam command. 
    fixed a bug which caused it to break down.

--- commands/spam.js
+++ commands/spam.js
@@ -4,18 +4,18 @@ module.exports = {
 	execute(client, message, args) {
 		//let spam_channel = message.guild.channels.find("name", "spam");
     	//if(!spam_channel) return message.channel.send("This command can only be used in spam channel!");
-    	let spam_channel = message.guild.channels.find(c => c.name.match(/spam/g))
-    	if(message.channel != spam_channel) {
-    		message.channel.send("This command can only be used in the channel named 'spam'.");
-    	}
-    	else {
-    		let spam_no = args[0];
-    		if(!spam_no) return message.channel.send("You didn't provide how much to spam!");
+        var spam_channel;
+        spam_channel = message.guild.channels.find(c => c.name.match(/spam/g))
+        let spam_no = args[0];
+        let spam_split = args.join(" ")
+        let spam_message = spam_split.split(spam_no).join(" ");
+        if (spam_channel) {
+            //if(!spam_no) return message.channel.send("You didn't provide how much to spam!");
             if(!spam_no) return message.channel.send(`Correct usage is:- k!spam <limit> <message>`).then(m => {m.delete(8000)});
-            if(spam_no > 99) return message.channel.send("Can't spam more than 99 message in once").then(m => {m.delete(4000)});
-			let spam_split = args.join(" ")
-			let spam_message = spam_split.split(spam_no).join(" ");
-			for(var i=0; i <spam_no; i++) spam_channel.send(spam_message);
-    	}
+            if(spam_no > 99) return message.channel.send("Can't spam more than 99 message in once").then(m => {m.delete(4000)});            for(var i=0; i <spam_no; i++) spam_channel.send(spam_message);
+        }
+        else {
+            message.channel.send("This command can only be used in the channel named 'spam'.")
+        }
 	},
 };
