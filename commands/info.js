1647a79ea2027eaf8377c45f2c10a104e286ab8b
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-11 13:11:16 +0530

    Update info.js
    updated the info command.
    added: spam command fixed and now working.

--- commands/info.js
+++ commands/info.js
@@ -5,11 +5,11 @@ module.exports = {
     description: 'get info about the bot',
     execute(client, message, args) {
     	let embed = new RichEmbed()
-    	.setTitle("Bisly v2.0.2")
+    	.setTitle("Bisly v2.0.3")
     	.setColor('#F70827')
     	.setDescription(`Bisly is a general purpose discord bot which is developed with the view of performing all general tasks in mind.\n
     	The bot contains commands for **Fun**, **Moderation**, **Misc** and **General** tasks.\n
-        In this update, the bot is added up with a GIF command.`)
+        In this update, we have fixed a bug in spam command, which caused it to break and does not work. Now it can spam in a certain channel, which has been named 'spam' or contains the same word.`)
     	.addField("Webpage:", "https://bisly.glitch.me")
     	.addField("Github repository:", "[Bisly on Github](https://github.com/spacesanjeet/Bisly)")
     	.setFooter("spacesanjeet#1363")
