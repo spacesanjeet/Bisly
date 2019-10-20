977aec9a6a45df6f183007139f0adbe24319dbfd
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-20 19:12:26 +0530

    Update ascii_art.js
    updated the ASCII command with a better code

--- commands/ascii_art.js
+++ commands/ascii_art.js
@@ -4,10 +4,13 @@ module.exports = {
     name: 'ascii',
     description: 'gets you a ascii art',
     execute(client, message, args) {
-        if(args.join(' ').length > 16) return message.channel.send('Only 16 characters are admitted!')
-        if (!args.join(' ')) return message.channel.send('Please, provide text to format in ASCII! Usage: ascii <text>').then(msg => msg.delete({timeout: 10000}));
-        figlet(args.join(' '), (err, data) => {
-            message.channel.send('```' + data + '```')
+        if (!args.join(' ')) 
+        	message.channel.send('Please, provide text to format in ASCII! Usage: ascii <text>').then(msg => {msg.delete(8000)});
+        else if (args.join(' ').length > 16) 
+        	message.channel.send('Only 16 characters are admitted!')
+        else
+        	figlet(args.join(' '), (err, data) => {
+            	message.channel.send('```' + data + '```')
         });
     },
 };
