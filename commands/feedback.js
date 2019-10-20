071b69196b518af8b7ab462db8ca916a67e6dbd7
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-20 19:02:52 +0530

    Update feedback.js
    fixed the overwrite error code in the feedback command.

--- commands/feedback.js
+++ commands/feedback.js
@@ -1,13 +1,3 @@
-module.exports = {
-	name: 'feedback',
-	description: 'gets feedback from users',
-	execute(client, message, args) {
-		if(!args.join(" ")) return message.channel.send("Specify a feedback please!");
-		let feedback = args.join(" ");
-		client.guilds.get("496197306882719755").channels.get("582068551633076231").send(`${feedback} by ${message.author.name} from ${message.guild.name}`);
-	},
-};
-
 const { RichEmbed } = require('discord.js');
 module.exports = {
 	name: 'feedback',
