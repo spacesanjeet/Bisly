dc2c904f04f6abdfe233f4e5693f7000e41d7d1c
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-20 19:07:10 +0530

    Update spam.js
    fixed the wrong indentation

--- commands/spam.js
+++ commands/spam.js
@@ -11,8 +11,9 @@ module.exports = {
         let spam_message = spam_split.split(spam_no).join(" ");
         if (spam_channel) {
             //if(!spam_no) return message.channel.send("You didn't provide how much to spam!");
-            if(!spam_no) return message.channel.send(`Correct usage is:- k!spam <limit> <message>`).then(m => {m.delete(8000)});
-            if(spam_no > 99) return message.channel.send("Can't spam more than 99 message in once").then(m => {m.delete(4000)});            for(var i=0; i <spam_no; i++) spam_channel.send(spam_message);
+            if(!spam_no) return message.channel.send(`Correct usage is:- b!spam <limit> <message>`).then(m => {m.delete(8000)});
+            if(spam_no > 99) return message.channel.send("Can't spam more than 99 message in once").then(m => {m.delete(4000)});            
+	    for(var i=0; i <spam_no; i++) spam_channel.send(spam_message);
         }
         else {
             message.channel.send("This command can only be used in the channel named 'spam'.")
