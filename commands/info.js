e2f0b6271f19df56793b047e7ea857cd4d4565d4
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-20 19:21:51 +0530

    Update info.js
    updated the info command with new fixes in the ASCII command

--- commands/info.js
+++ commands/info.js
@@ -5,11 +5,11 @@ module.exports = {
     description: 'get info about the bot',
     execute(client, message, args) {
     	let embed = new RichEmbed()
-    	.setTitle("Bisly v2.0.3")
+    	.setTitle("Bisly v2.0.5")
     	.setColor('#F70827')
     	.setDescription(`Bisly is a general purpose discord bot which is developed with the view of performing all general tasks in mind.\n
     	The bot contains commands for **Fun**, **Moderation**, **Misc** and **General** tasks.\n
-        In this update, we have added a thumbnail to the Lyrics command, now it can show thumbnails to what lyrics you have searched for.`)
+        In this small new update, we have fixed several issues in the 'ascii' command to make it more effective.`)
     	.addField("Webpage:", "https://bisly.glitch.me")
     	.addField("Github repository:", "[Bisly on Github](https://github.com/spacesanjeet/Bisly)")
     	.setFooter("spacesanjeet#1363")
