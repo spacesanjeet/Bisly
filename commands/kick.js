5da5093faebbb846dd9475d867016260534c639d
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-21 22:34:55 +0530

    updated kick command
    fixed the code, making it more efficient

--- commands/kick.js
+++ commands/kick.js
@@ -3,22 +3,15 @@ const { RichEmbed } = require('discord.js');
 module.exports = {
     name: 'kick',
     description: 'kick a user with reason',
-    execute(client, message, args) {
-        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!kUser) return message.channel.send("Can't find the user!");
-        let join = args.join(" ");
-        let kReason = join.split(kUser).join(" ")
+    async execute(client, message, args) {
         if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You don't have permissions to use this command!");
-    
-        let kickEmbed = new RichEmbed()
-        .setDescription("Member Kicked")
-        .setColor("RANDOM")
-        .addField("Kicked User:", `${kUser} with ID ${kUser.id}`)
-        .addField("Kicked by:", `${message.author} with ID ${message.author.id}`)
-        .addField("Time:", message.createdAt)
-        .addField("Reason:", kReason);
-    
-        message.guild.member(kUser).kick(kReason);
-        message.channel.send(kickEmbed);
+        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(!kUser) return message.channel.send("Can't find that user!");
+        if(!kUser.kickable) return message.channel.send("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+        let kReason = args.slice(1).join(' ');
+        if(!kReason) kReason = "No reason provided";
+        await kUser.kick(kReason)
+            .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+        message.channel.send(`**${kUser.user.tag}** has been kicked by **${message.author.tag}** because of **${kReason}**`);
     },
 };
