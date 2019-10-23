d15c5a682b4a4de5059775924ed59327a81fcdd1
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-10-23 19:37:46 +0530

    updated spam.js
    added support for when the user doesn't inputs message for the spam

--- commands/spam.js
+++ commands/spam.js
@@ -1,3 +1,5 @@
+const prefix = process.env.PREFIX;
+
 module.exports = {
 	name: 'spam',
 	description: 'spam certain messages in a certain channel',
@@ -7,13 +9,15 @@ module.exports = {
         var spam_channel;
         spam_channel = message.guild.channels.find(c => c.name.match(/spam/g))
         let spam_no = args[0];
-        let spam_split = args.join(" ")
-        let spam_message = spam_split.split(spam_no).join(" ");
+        //let spam_split = args.join(" ")
+        let spam_message = args.slice(1).join(' ');
+        //let spam_message = spam_split.split(spam_no).join(" ");
         if (spam_channel) {
             //if(!spam_no) return message.channel.send("You didn't provide how much to spam!");
-            if(!spam_no) return message.channel.send(`Correct usage is:- b!spam <limit> <message>`).then(m => {m.delete(8000)});
-            if(spam_no > 99) return message.channel.send("Can't spam more than 99 message in once").then(m => {m.delete(4000)});            
-	    for(var i=0; i <spam_no; i++) spam_channel.send(spam_message);
+            if(!spam_no) return message.channel.send(`Correct usage is:- ${prefix}spam <limit> <message>`).then(m => {m.delete(8000)});
+            if(!spam_message) return message.channel.send(`Correct usage is:- ${prefix}spam <limit> <message>`).then(m => {m.delete(8000)});
+            if(spam_no > 99) return message.channel.send("Can't spam more than 99 messages in once").then(m => {m.delete(4000)});
+            for(var i=0; i <spam_no; i++) spam_channel.send(spam_message);
         }
         else {
             message.channel.send("This command can only be used in the channel named 'spam'.")
