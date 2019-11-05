889dfb76826f825d4548558962467460ea940735
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-11-05 19:04:17 +0530

    updated info.js
    new version up and running

--- commands/info.js
+++ commands/info.js
@@ -5,14 +5,14 @@ module.exports = {
     description: 'get info about the bot',
     execute(client, message, args) {
     	let embed = new RichEmbed()
-    	.setTitle("Bisly v2.0.7")
+    	.setTitle("Bisly v2.0.8")
     	.setColor('#F70827')
     	.setDescription(`Bisly is a general purpose discord bot which is developed with the view of performing all general tasks in mind.\n
-    	The bot contains commands for **Fun**, **Moderation**, **Misc** and **General** tasks.\n
-        In this update, we have fixed the spam command to work more effectively.`)
+    		The bot contains commands for **Fun**, **Moderation**, **Misc** and **General** tasks.\n
+        In this update, we have fixed a bug in user command, which caused it to not work at times.`)
     	.addField("Webpage:", "https://bisly.glitch.me")
     	.addField("Github repository:", "[Bisly on Github](https://github.com/spacesanjeet/Bisly)")
     	.setFooter("spacesanjeet#1363")
     	message.channel.send(embed)
-	},
+		},
 };
