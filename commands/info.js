df712117ec9c5c8a6ac0f6c9dcfae93024700ce0
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2019-11-19 20:38:30 +0530

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -5,11 +5,11 @@ module.exports = {
     description: 'get info about the bot',
     execute(client, message, args) {
     	let embed = new RichEmbed()
-    	.setTitle("Bisly v2.0.8")
+    	.setTitle("Bisly v2.0.9")
     	.setColor('#F70827')
     	.setDescription(`Bisly is a general purpose discord bot which is developed with the view of performing all general tasks in mind.\n
     		The bot contains commands for **Fun**, **Moderation**, **Misc** and **General** tasks.\n
-        In this update, we have fixed a bug in user command, which caused it to not work at times.`)
+        In this update, we have added a new command, named cinvite, which can be used to create temporary server invites.`)
     	.addField("Webpage:", "https://bisly.glitch.me")
     	.addField("Github repository:", "[Bisly on Github](https://github.com/spacesanjeet/Bisly)")
     	.setFooter("spacesanjeet#1363")
