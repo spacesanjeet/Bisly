e34a157be918415a194789df80d4a3fd582f322c
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2020-03-29 13:51:21 +0530

    Update info.js
    updated the info command

--- commands/info.js
+++ commands/info.js
@@ -14,7 +14,8 @@ module.exports = {
         .setDescription(`Bisly is a general purpose discord bot which is developed with the view of performing all general tasks in mind.\n
         The bot contains commands for **Fun**, **Moderation**, **Misc** and **General** tasks.\n
         In this update the source code has been rewritten completely. For further changes, consider checking the Bisly's Github.`)
-        .addField("Webpage:", "https://bisly.glitch.me")
+        .addField("Development Server: ", "[Bisly's Home](https://discord.gg/B83ScTT)")
+        .addField("Webpage:", "[BislyWeb](https://bisly.glitch.me)")
         .addField("Github repository:", "[Bisly on Github](https://github.com/spacesanjeet/Bisly)")
         .setFooter("spacesanjeet#1363")
         message.channel.send(embed)
