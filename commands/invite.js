3ffa5bac911bd1d15888ae5d31c391db908d8aea
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2020-04-25 13:34:30 +0530

    Update invite.js
    changed and corrected the permissions the bot needs to function

--- commands/invite.js
+++ commands/invite.js
@@ -10,7 +10,7 @@ module.exports = {
         let embed = new RichEmbed()
         .setColor('RANDOM')
         .setTitle('Invite Link ❤')
-        .setURL("https://discordapp.com/oauth2/authorize?client_id=496198253193461792&scope=bot&permissions=1342254327")
+        .setURL("https://discordapp.com/oauth2/authorize?client_id=496198253193461792&scope=bot&permissions=93399")
         .setTimestamp(new Date())
         .setFooter('Requested by: ' + message.author.username)
         message.channel.send(embed)
