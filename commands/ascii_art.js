fb12b509a57ef0fdd9988868c95e90d5b99d5063
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2020-05-05 23:31:32 +0530

    Update ascii_art.js

--- commands/ascii_art.js
+++ commands/ascii_art.js
@@ -6,12 +6,13 @@ module.exports = {
   guildOnly: true,
 	aliases: ['art'],
   usage: '[string]',
+  example: 'spacesanjeet',
   cooldown: 3,
-	execute(client, message, args) {
+  execute(client, message, args) {
     	if(args.join(' ').length > 16) return message.channel.send('Only 16 characters are admitted!')
-      	if (!args.join(' ')) return message.channel.send('Please, provide text to format in ASCII! Usage: ascii <text>').then(msg => msg.delete({timeout: 10000}));
-      	figlet(args.join(' '), (err, data) => {
+      if (!args.join(' ')) return message.channel.send('Please, provide text to format in ASCII! Usage: ascii <text>').then(msg => msg.delete({timeout: 10000}));
+      figlet(args.join(' '), (err, data) => {
           message.channel.send('```' + data + '```')
-      	});
-  	},
+      });
+  },
 };
