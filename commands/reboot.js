6328d3662abb165997e773f00b9c1e507e0890a8
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2020-05-05 23:50:19 +0530

    Update reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,18 +1,16 @@
- const token = process.env.TOKEN;
+const token = process.env.TOKEN;
 const id = process.env.OwnerID;
 
 module.exports = {
-	name: 'reboot',
-	description: 'Reloads the bot',
-    guildOnly: true,
-	aliases: ['reset', 'logout'],
-    usage: '',
-    cooldown: 3,
-    execute(client, message, args) {
-		if (!message.author.id == id) return message.channel.send(`You can't use this command!`);
-		client.destroy() //destroying the bot login status
-    	client.login(process.env.TOKEN) //logging back in into discord
-    	message.channel.send(`Rebooted successfully. API latency is ${Math.round(client.ping)}ms`);
-    },
+  name: "reboot",
+  description: "Reloads the bot",
+  guildOnly: true,
+  aliases: ["reset", "logout"],
+  cooldown: 3,
+  execute(client, message, args) {
+    if (!message.author.id == id) return message.channel.send(`You can't use this command!`);
+    client.destroy();
+    client.login(token);
+    message.channel.send(`Rebooted successfully. API latency is ${Math.round(client.ping)}ms`);
+  }
 };
-
