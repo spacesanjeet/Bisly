8128c504a4715301f2c636d4c9ca5da0f58b872d
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2020-05-05 23:47:45 +0530

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -5,10 +5,10 @@ module.exports = {
     name: 'meme',
     description: 'Get random memes',
     guildOnly: true,
-    usage: '[command]',
+    usage: ' ',
     cooldown: 5,
     async execute(client, message, args) {
-        const m = await message.channel.send("Meme on the way!");
+        const m = await message.channel.send("Meme on the way!" );
         meme('memes', function(err, data) {
             let embed = new RichEmbed()
             .setTitle(data.title)
