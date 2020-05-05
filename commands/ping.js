3d9d49f06253121460fdb467b0dd9ffbb851d783
Sanjeet Patel <43694245+spacesanjeet@users.noreply.github.com>
2020-05-05 23:48:30 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     description: "Check the bot's latency",
     guildOnly: true,
-    usage: '[command]',
+    usage: ' ',
     async execute(client, message, args) {
         const m = await message.channel.send("Ping!");
         m.edit(`Pong! Response Took ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
